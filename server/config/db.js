9283f39f6701ebcf20c7724ee8b1ba9b90d5fb6b
Clayton Rapiejko <83980609+ClaytonRapiejko@users.noreply.github.com>
2021-06-16 23:25:08 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://Clayton:test@cluster0.e9y6a.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
